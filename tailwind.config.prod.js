e7a31e4748183b207194ca0cbed9375d11c7395b
lanyusan <fqye@daima.co>
2021-04-05 09:56:41 +0800

    Initial

---
+++ tailwind.config.prod.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  purge: ['pages/**/*.html', 'pages/**/*.ejs', 'src/**/*.ejs'], // or other files for tail wind production build
+  future: {},
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
